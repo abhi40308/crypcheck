5c51cdc9138aac7fa8d8a1745efdc2cc8efc3985
abhijeet <abhijeet40308@gmail.com>
2018-04-29 00:02:08 +0530

    add styling

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private year = (new Date()).getFullYear();
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
