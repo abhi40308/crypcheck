a26600320a6ba6e77f6e19b49fc29f7b00c5b192
abhijeet <abhijeet40308@gmail.com>
2018-04-29 01:07:20 +0530

    data refresh

--- src/app/crypdata.service.ts
+++ src/app/crypdata.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class CrypdataService {
@@ -16,4 +18,14 @@ export class CrypdataService {
   getall(){
 	  return this.http.get(`https://api.coinmarketcap.com/v1/ticker/`).map((res:Response) => res.json());
   }
+
+  public data$: BehaviorSubject<any> = new BehaviorSubject({});
+
+  updateData() {
+    let data = this.http.get('https://api.coinmarketcap.com/v1/ticker/').map((data)=>{
+        return data.json();
+    }).do((data)=>{
+        this.data$.next(data);
+    })
+}
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -10,12 +10,22 @@ import { CrypdataService } from '../crypdata.service'
 export class TableComponent implements OnInit {
 
   private currency=[];
+  private interval: any;
 
   constructor( private crypdataservice: CrypdataService ) { }
 
   ngOnInit() {
   this.crypdataservice.getall().subscribe(data => this.currency = data);
+
+  this.refreshData();
+  this.interval = setInterval(() => { 
+        this.refreshData(); 
+    }, 300000);
   }
+
+  refreshData(){
+    this.crypdataservice.updateData(); 
+}
 
 
 }
